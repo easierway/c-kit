f1fc07593b6e7535e3f37245ed5894001363b3b8
hatlonely <hatlonely@gmail.com>
2018-07-26 15:03:54 +0800

    update

---
+++ test/balancer/test_balancer.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(testBalancer, case1) {
+    std::cout << "hello world" << std::endl;
+}
