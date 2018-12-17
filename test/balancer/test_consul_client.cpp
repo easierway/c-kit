d80af09b262e28fbae3351f3d71d853b0fa97d30
leon <leon@mobvista.com>
2018-12-17 09:22:33 +0000

    add consul client test

--- test/balancer/test_consul_client.cpp
+++ test/balancer/test_consul_client.cpp
@@ -8,24 +8,53 @@
 #include <log4cplus/loggingmacros.h>
 
 int main(int argc, char *argv[]) {
+    log4cplus::initialize();
+    log4cplus::BasicConfigurator config;
+    config.configure();
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
 namespace kit {
 
-TEST(testBalancer, caseConsulResolver) {
-    log4cplus::PropertyConfigurator config(LOG4CPLUS_C_STR_TO_TSTRING("log.ini"));
-    config.configure();
-    log4cplus::Logger logger = log4cplus::Logger::getInstance("info");
+TEST(testConsulClient, caseGetKV) {
+    log4cplus::Logger logger = log4cplus::Logger::getInstance("test");
 
     auto client = std::make_shared<ConsulClient>("http://sg-consul.mobvista.com:8500");
     int status = -1;
     json11::Json kv;
     std::string err;
     std::string lastIndex;
-    std::tie(status, kv, err) = client->GetKV("rs/zone_cpu.json", 10, lastIndex);
-    LOG4CPLUS_INFO(logger, "rs/zone_cpu.json" << kv.dump());
+
+    // zone cpu
+    std::tie(status, kv, err) = client->GetKV("clb/rs/zone_cpu.json", 10, lastIndex);
+    GTEST_ASSERT_EQ(0, status);
+    GTEST_ASSERT_EQ("", err);
+    LOG4CPLUS_DEBUG(logger, "kv/clb/rs/zone_cpu.json: [" << kv.dump() << "]");
+
+    // instance factor
+    std::tie(status, kv, err) = client->GetKV("clb/rs/instance_factor.json", 10, lastIndex);
+    GTEST_ASSERT_EQ(0, status);
+    GTEST_ASSERT_EQ("", err);
+    LOG4CPLUS_DEBUG(logger, "kv/clb/rs/instance_factor.json: [" << kv.dump() << "]");
+
+}
+
+TEST(testConsulClient, caseGetService) {
+    log4cplus::Logger logger = log4cplus::Logger::getInstance("test");
+
+    auto client = std::make_shared<ConsulClient>("http://sg-consul.mobvista.com:8500");
+    int status = -1;
+    json11::Json kv;
+    std::vector<std::shared_ptr<ServiceNode>> nodes;
+    std::string err;
+    std::string lastIndex;
+
+    // rs
+    std::tie(status, nodes, err) = client->GetService("rs", 10, lastIndex);
+    GTEST_ASSERT_EQ(0, status);
+    GTEST_ASSERT_EQ("", err);
+    LOG4CPLUS_DEBUG(logger, "health/service/rs: [" << nodes.size() << "]");
 }
 
 }
